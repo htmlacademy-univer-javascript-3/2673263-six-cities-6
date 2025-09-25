cb995ae5ddb0b47919e194ffeec3832e16f7ec7e
Дмитрий <145160988+lyubdim@users.noreply.github.com>
2025-09-25 23:11:14 +0500

    module2-task1

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,26 +1,31 @@
-import OfferCard from "../../components/offer-card/offer-card"
+import OfferCard from '../../components/offer-card/offer-card';
 
 type MainPageProps = {
   offersCount: number;
 };
 
 function MainPage({ offersCount }: MainPageProps): JSX.Element {
-    return (
-        <div className="page page--gray page--main">
+  return (
+    <div className="page page--gray page--main">
       <header className="header">
         <div className="container">
           <div className="header__wrapper">
             <div className="header__left">
               <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
+                <img
+                  className="header__logo"
+                  src="img/logo.svg"
+                  alt="6 cities logo"
+                  width="81"
+                  height="41"
+                />
               </a>
             </div>
             <nav className="header__nav">
               <ul className="header__nav-list">
                 <li className="header__nav-item user">
                   <a className="header__nav-link header__nav-link--profile" href="#">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
+                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
                     <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
                     <span className="header__favorite-count">3</span>
                   </a>
@@ -57,6 +62,7 @@ function MainPage({ offersCount }: MainPageProps): JSX.Element {
                 </a>
               </li>
               <li className="locations__item">
+                {/* допускается без href, оставлю как в макете */}
                 <a className="locations__item-link tabs__item tabs__item--active">
                   <span>Amsterdam</span>
                 </a>
@@ -74,6 +80,7 @@ function MainPage({ offersCount }: MainPageProps): JSX.Element {
             </ul>
           </section>
         </div>
+
         <div className="cities">
           <div className="cities__places-container container">
             <section className="cities__places places">
@@ -88,12 +95,15 @@ function MainPage({ offersCount }: MainPageProps): JSX.Element {
                   </svg>
                 </span>
                 <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex={0}>Popular</li>
+                  <li className="places__option places__option--active" tabIndex={0}>
+                    Popular
+                  </li>
                   <li className="places__option" tabIndex={0}>Price: low to high</li>
                   <li className="places__option" tabIndex={0}>Price: high to low</li>
                   <li className="places__option" tabIndex={0}>Top rated first</li>
                 </ul>
               </form>
+
               <div className="cities__places-list places__list tabs__content">
                 <OfferCard />
                 <OfferCard />
@@ -110,7 +120,7 @@ function MainPage({ offersCount }: MainPageProps): JSX.Element {
         </div>
       </main>
     </div>
-    )
+  );
 }
 
 export default MainPage;
